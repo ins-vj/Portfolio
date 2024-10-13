c01c8ed31cd516e0321441a4e6dac29dbbf0884e
ins-vj <163007537+ins-vj@users.noreply.github.com>
2024-10-13 22:10:33 +0530

    Update design2.tsx

--- src/components/design2.tsx
+++ src/components/design2.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef} from 'react';
 import { motion } from 'framer-motion';
 import {
   SiCplusplus,
