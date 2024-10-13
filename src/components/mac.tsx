d444488044593b764bc012d80f4cadee67493cb2
ins-vj <163007537+ins-vj@users.noreply.github.com>
2024-10-13 22:19:25 +0530

    Update mac.tsx

--- src/components/mac.tsx
+++ src/components/mac.tsx
@@ -26,7 +26,6 @@ import { IconCaretDownFilled } from "@tabler/icons-react";
 import Image from "next/image";
 
 export const MacbookScroll = ({
-  src,
   showGradient,
   title,
   badge,
@@ -87,7 +86,7 @@ const textOpacity = useTransform(scrollYProgress, [0, 0.1], [1, 0]);
       >
         {title || (
           <span>
-          VIKRANT <br /> IIT Indore CSE'27
+          VIKRANT <br /> IIT Indore CSE&#39;27
           </span>
         )}
       </motion.h2>
@@ -653,23 +652,4 @@ export const OptionKey = ({ className }: { className: string }) => {
   );
 };
 
-const AceternityLogo = () => {
-  return (
-    <svg
-      width="66"
-      height="65"
-      viewBox="0 0 66 65"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-3 w-3 text-white"
-    >
-      <path
-        d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
-        stroke="currentColor"
-        strokeWidth="15"
-        strokeMiterlimit="3.86874"
-        strokeLinecap="round"
-      />
-    </svg>
-  );
-};
+
